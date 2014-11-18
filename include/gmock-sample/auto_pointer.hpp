96e129c84f6c398fb13fe19dbf4d48630a72b702
Kestutis Baltronas <kestutisbalt@gmail.com>
2014-11-18 22:33:08 +0200

    Added get and reset functionality.

--- include/gmock-sample/auto_pointer.hpp
+++ include/gmock-sample/auto_pointer.hpp
@@ -12,6 +12,9 @@ public:
 
 	~auto_pointer();
 
+	T *get();
+
+	void reset(T *new_pointer = nullptr);
 private:
 	T *pointer_;
 };
--- src/auto_pointer.cpp
+++ src/auto_pointer.cpp
@@ -15,4 +15,19 @@ auto_pointer<T>::~auto_pointer()
 	delete pointer_;
 }
 
+
+template <class T>
+T *auto_pointer<T>::get()
+{
+	return pointer_;
+}
+
+
+template <class T>
+void auto_pointer<T>::reset(T *new_pointer)
+{
+	delete pointer_;
+	pointer_ = new_pointer;
+}
+
 }
